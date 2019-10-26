0cfe1df216b10f1c81650d6339427ec0a0572a3b
JhordanRojas <56855169+JhordanRojas@users.noreply.github.com>
2019-10-25 19:47:56 -0500

    Add files via upload

---
+++ RojasLizana.py
@@ -0,0 +1,343 @@
+#Agrupacion 1
+nombre_alumno="Jhordan Rojas"
+codigo_alumno="192032D"
+escuela="Ingenieria Electronica"
+print("El alumno:", nombre_alumno, "tiene como codigo:", codigo_alumno, "y estudia en la escuela de", escuela)
+#Agrupacion 2
+empresa="Arquicom"
+razon_social="E.I.R.L"
+direccion="Elias Aguirre 250"
+print("La empresa", empresa, "es una", razon_social, "que se ubica en", direccion)
+#Agrupacion 3
+numero_tomo="1"
+numero_paginas="771"
+isbn="978-612-4160-00-4"
+print("El libro de analisis matematico", "de numero de tomo", numero_tomo, "tiene", numero_paginas, "paginas", "y un I.S.B.N de numero", isbn )
+#Agrupacion 4
+fecha_emision="10/03/2016"
+personaje="Jose Abelardo Quiñones"
+predom_color="verde"
+print("El billete de diez soles tine fecha de emision:", fecha_emision, "y tiene como personaje impreso al heroe", personaje,", una de sus caracterisitcas es que tiene como color predominate el", predom_color)
+#Agrupacion 5
+peso="170"
+memoria="32"
+megapixel_cam="13"
+print("El galaxy a10 es un smartphone que pesa:", peso, "con una memoria de", memoria,"GB, y una camara de", megapixel_cam,"megapixeles")
+#Agrupacion 6
+ex_medicos_monto="180"
+fecha_pagoo="25/09/2019"
+dni="7508517"
+print("El alumno con dni:", dni,"pago el monto de", ex_medicos_monto, "el dia", fecha_pagoo,"para la realizacion de sus examenes medicos")
+#Agrupacion 7
+promulgacion="1993"
+funcion="tecto constitucional"
+autor="congreso constituyente"
+print("la constitucion politica del Peru es un", funcion, "que fue creado por el", autor, "y promulgado el año", promulgacion)
+#Agrupacion 8
+numer_titulos="6"
+numer_cap="26"
+numer_art="206"
+print("La constitucion del Peru consta de", numer_titulos, "titulos", "que a su vez tiene", numer_cap,"capitulos","y se divide en", numer_art,"articulos")
+#Agrupacion 9
+grasas="7.5"
+proteinas="6"
+carbohidartos="10"
+print("La leche gloria tiene", grasas,"gramos de grasas",",", proteinas,"gramos de proteinas", "y", carbohidartos, "grmos de carbohidratos")
+#Agrupacion 10
+vit_c="5"
+vit_d="1,5"
+vit_a="240"
+print("Leche gloria tiene:", vit_c, "miligramos de vitamina C",",",vit_d,"micro gramos de vitamina D", "y", vit_a, "de vitamina A")
+#Agrupacion 11
+nmero_bol="31161"
+nmero_ruc="20480713681"
+telefono="227282"
+print("La empresa Arquicom, con numero de ruc:", nmero_ruc, "y telefono:", telefono, "ha emititdo su boleta numero", nmero_bol)
+#Agrupacion 12
+n_elementos="118"
+naturales="94"
+artificiales="24"
+print("la tabla de los elemntos quimicos consta de:", n_elementos, "elementos totales, de los cuales",naturales, "son naturales y", artificiales, "son artificiales")
+#Agrupacion 13
+periodos="7"
+grupos="18"
+bloques="4"
+print("La tabla periodica se conforma por:", bloques, "bloques",",", periodos," periodos","y", grupos,"grupos")
+#Agrupacion 14
+tipo_doc="Acuerdo comercial"
+vigente="01/02/2012"
+firmado="06/04/2011"
+print("El tlc Peru - Mexico es un", tipo_doc,"que fue firmado el", firmado, "y se encuentra vigente desde", vigente)
+#Agrupacion 15
+estado="vigente"
+num_prods="12000"
+tipo="TLC"
+print("Entre peru y mexico se desarrolla un",tipo,"que actualmente se encuentra en estado", estado,"el cual ayuda a mejorar la comercializacion de", num_prods,"productos")
+#Agrupacion 16
+doc_tip="tratado"
+firma_cap="09/11/1824"
+fucncion="Reconocimento de independencia"
+print("La capitulacion de toledo es un", doc_tip, "que se firmo el", firma_cap,"y cumple con el", fucncion)
+#Agrupacion 17
+caracter="definitivo"
+lugar="ayacucho"
+est="vigente"
+print("la capitulacion de ayacucho se dio en", lugar, "en la sierra de peru","es de caracter", caracter,"y tiene un estado", estado)
+#Agrupacion 18
+boleta_num="77"
+ruc_num="10734767501"
+empressa="Dhilya medic"
+print("la empresa", empressa, "tiene el ruc de numero", ruc_num, "y produce la boleta numero", boleta_num)
+#Agrupacion 19
+rubro="Bordados"
+direc="Vicente de la vega 876"
+tele="947256162"
+print("Dhilya medic se dedica a los", rubro, " y se ubica en", direc,", se puede contactar con el numero", tele)
+#Agrupacion 20
+emisor="ENSA"
+mess="agosto"
+num_recibo="251-38747618"
+print("la empresa de servicio electrico", emisor,"emitio el recibo numero", num_recibo,"correspondiente al mes de", mess,)
+#Agrupacion 21
+medidor_num="607383563"
+consumo="94 kWh"
+tip_conex="Monofásica-aérea"
+print("el medidor de numero", medidor_num, "tiene una conexion de tipo", tip_conex, " y ha tenido un consumo de", consumo)
+#Agrupacion 22
+ruc_ensa="20103117560"
+consultas_cod="26152132"
+tarifa="BT5B-residencial"
+print("la empresa ensa de ruc", ruc_ensa,"utiliza la opcion tarifaria", tarifa, "para el recibo, tiene el numero de consultas:", consultas_cod)
+#Agrupacion 23
+univ="Pedro Ruiz Gallo"
+anio="2019"
+pags="56"
+print("la universidad", univ, "ha publicado el prospecto de admision de año", anio, "en un documento de", pags, "paginas")
+#Agrupacion 24
+ubic="Lambayeque"
+publicado="Oficina General de Admisión"
+ccaps="14"
+print("la universidad esta ubicada en", ubic,"ha publicado a traves de la", publicado,"un prospecto de admision que consta de", ccaps)
+#Agrupacion 25
+parts="Reglamento y cuestionario"
+arts="100"
+grups="5"
+print("este prospecto de admision presenta dos partes:", parts,", que consta de", arts, "articulos, para los", grups, "grupos a evaluarse")
+#Agrupacion 26
+autorr="Nicolas Maquiavelo"
+caps_book="26"
+nums_pags="103"
+print("El libro el principe fue escrita por", autorr, "este libro tiene", caps_book, "capitulos, que forma un total de", nums_pags, "paginas")
+#Agrupacion 27
+genero="Ensayo"
+pais_origen="Italia"
+publicacion="1532"
+print("El principe es un libro que pertenece al genero",genero,", fue publicado en", pais_origen,"en el año", publicacion)
+#Agrupacion 28
+text_tipe="Documento declarativo"
+signers="Paises de las naciones unidas"
+ratificacion="10/12/1948"
+print("La declaracion de los derechos humanos es un", text_tipe, "firmado por", signers, "la fecha:", ratificacion)
+#Agrupacion 29
+paiss="Francia"
+articuos="30"
+resolucioon="217A(III)"
+print("la dclaracion de los derechos humanos fue firmado en",paiss,"consta de", articuos, "y fue aprobado mediante la", resolucioon)
+#Agrupacion 30
+numero_libreta="65936"
+libro="3"
+folio="77"
+print("la libreta numero:", numero_libreta, "se inscribio en el libro", libro, "y en el folio", folio)
+#Agrupacion 31
+docc="Constancia de inscripcion"
+clasifica="Disponible"
+ocupp="Estudiante"
+print("Este documento que es una", docc,"clasifica al dueño de dcoumento como", clasifica, "el cual se desarrolla como", ocupp)
+#Agrupacion 32
+republica="Peruana"
+typee="Identificacion"
+emisso="RENIEC"
+print("El D.N.I es un documento de la reupblica", republica, "que sirve como", typee, "y es emitido por",emisso)
+#Agrupacion 33
+dni_num="75082517"
+inscrip_fecha="29/10/2010"
+emition_fecha="02/09/2019"
+print("El DNI numero", dni_num, "fue inscrito en la fecha", inscrip_fecha,"y emitido por la RENIEC el", emition_fecha)
+#Agrupacion 34
+departamento="Lambayeque"
+provincia="Chiclayo"
+votacion_grup="283419"
+print("el DNI ", dni_num,"pertenece al departamento de", departamento,"a la provincia de", provincia,"y tiene como grupo de votacion", votacion_grup)
+#Agrupacion 35
+medic_tipe="Suspensión oral"
+elab="Cifarma S.A."
+distrib="Deutsche pharma"
+print("El medicamneto purinator es de tipo", medic_tipe, "el cual es elaborado por", elab, "y distribuido por,",distrib)
+#Agrupacion 36
+cont="100"
+indicacion="Suplemento vitamínico"
+contrain="Ninguna"
+print("El medicamento Purinator viene en contenido de:", cont,"indicado para su uso como", indicacion, "y con", contrain,"contraindicacion")
+#Agrupacion 37
+fabri_en="Perú"
+vent="Bajo receta médica"
+codig_bar="751940000447"
+print("Purinator es fabricado en", fabri_en,"se vende", vent,"el producto actual tiene codigo", codig_bar)
+#Agrupacion 38
+ruc_deu="20466635286"
+rsn="N-24180"
+dirr="jr. Rio de janeiro Nº339 Lima"
+print("purinator es producida por una empresa de ruc", ruc_deu,"que funciona bajo la resolucion", rsn, "y se ubica en", dirr,"en el peru")
+#Agrupacion 39
+tpi_bebida="Gaseosa"
+emp_encaragda="Coca cola company"
+empr_prod="Lindley S.A."
+print("La inka cola es una bebida del tipo", tpi_bebida, "que es producida por", empr_prod,"y que se distribuye a cargo de", emp_encaragda)
+#Agrupacion 40
+salida="1935"
+orig="Perú"
+color="Amarillo"
+print("La inka cola es una gaseosa que salio al mercado en", salida, "es originaria del", orig,"y es de color", color)
+#Agrupacion 41
+emitton="SUNARP"
+codigoo="E1837734"
+tipp="Solicitud"
+print("la inscripcion de titulo es un documento de tipo", tipp, "emitido por",emitton, "el presente tiene por codigo", codigoo)
+#Agrupacion 42
+doc_tipe="Tratado"
+firmn="29/06/1919"
+vigor="10/01/1920"
+print("El tratado de versalles es un documento de tipo", doc_tipe, "que feu firmado el", firmn, "y entro en vigor el", vigor)
+#Agrupacion 43
+nu_part="15"
+clausulas="13"
+artics_num="440"
+print("El tratado de versalles tiene", nu_part,"partes","consta de", clausulas,"clausulas, y de", artics_num,"articulos")
+#Agrupacion 44
+lug="Versalles"
+countr="Francia"
+prop="Finalizar la 1º guerra mundial"
+print("fue firmado en",lug, "en el pais de,",countr, "con el proposito de", prop)
+#Agrupacion 45
+dep="Gobierno de Francia"
+idiom="3"
+cond="ratificado por el imprio alemán"
+print("El tratado de versalles fue escito en", idiom,"idiomas", "el depositario es el", dep, "y esta en la condicion de", cond)
+#Agrupacion 46
+soft="Sistema operativo"
+launchh="29/07/2015"
+desingner="Microsoft"
+print("Windows 10 es un", soft, "lazando en la fecha", launchh,"y que fue desarrolado y diseñado por la empresa", desingner)
+#Agrupacion 47
+model="Software propietario"
+intr="fluent desing"
+idioms="Multilingue"
+print("Windows 10 esta desarrolado bajo el modelo de", model,", cuneta con la interfax grafica denominada", intr, "y funciona en version", idioms)
+#Agrupacion 48
+estade="En desarrollo"
+num_ver="8"
+actualiz="Microsoft update"
+print("Windos 10 permanece en el estado",estade, "debido asu actualizaciones a traves de", actualiz, "hasta la fecha cuenta con un total de", num_ver,"versiones")
+#Agrupacion 49
+ult_ver="1903"
+pred="Windows 8.1"
+soprte="Continuo"
+print("Su ultima version estable es la version", ult_ver, "su predecesor es", pred, "y su soporte es", soprte)
+#Agrupacion 50
+tip_soft="Aplicacion"
+serv="Streaming de musica"
+fundacion="2006"
+print("Spotify es un doftware del tipo", tip_soft,"fundada el año", fundacion, "y que ofrece el servicio de",serv)
+#Agrupacion 51
+numero_tarjet="3778 900099 76512"
+caducid="12/2021"
+tipot="Credito"
+print("Esta trajeta tiene como numero", numero_tarjet,"vence en la fecha", caducid, "y es del tipo", tipot)
+#Agrupacion 52
+generr="poesia"
+autrr="Cesar Vallejo"
+pub="1919"
+print("los heraldo negros es un libro que pertenece al genro de",generr,"pertenece al escritor peruano",autrr, "y fue publicada en el año",pub)
+#Agrupacion 53
+idom="Español"
+ubicac="Lima,Perú"
+poems="69"
+print("Los heraldos negros fue publicado en", idom,"en", ubicac, "este es un libro que consta de un total de", poems,"poemas")
+#Agrupacion 54
+corriente="Modernismo"
+seccione="6"
+editt="talleres de penitenciaria"
+print("Los heraldo negros pertenece a la corriente literaria del",corriente, "cuenta con", seccione, "secciones","y su editorial fue",editt)
+#Agrupacion 55
+conten=145
+elabb="AJE"
+bebd="jugo de durazno concentrado"
+print("Pulp es una bebida elaborada de", bebd, "elaborada por la empresa", elabb, "cuyo contenido es de", str(conten))
+#Agrupacion 56
+envasee="tetrapack"
+atencion="0800-10330"
+codidd=775067002073
+print("El envase de pulp tiene el codigo", str(codidd),"el tipo de envase es de",envasee, "y el numero de atencion al cliente es",atencion)
+#Agrupacion 57
+fabric="Apple"
+oss="Mac os"
+procesor="Intel Core i9"
+print("La macbook pro es un ordenador portatil fabricado por", fabric," cuenta con el sistema oprativo",oss,"y viene equipado con el procesador",procesor)
+#Agrupacion 58
+tip_disp="retina display"
+memo="512 GB"
+graficos="Radeon Pro 560X"
+print("La macbook pro viene equipada con la pantalla", tip_disp,"ademas de una memoria", memo, "y una tarjeta de graficos", graficos)
+#Agrupacion 59
+outor="Fiódor Dostoievski"
+estil="Novela psicologica"
+pubb=1886
+print("Crimen y castigo es una novela del autor", outor,"que pertence al estilo de", estil,"y fue publicada en el año", str(pub))
+#Agrupacion 60
+lang="ruso"
+pgs=426
+paiss="imperio ruso"
+print("Crimen y castigo fue publicado en el idioma", lang, "consta de un total de", str(pgs), "y fue escrito en el",paiss)
+#Agrupacion 61
+tema="asesinato y angustia"
+ambientada="san petersburgo"
+editor="El Mensajero Ruso"
+print("Crimen y castigo trata el tema de", tema,", la novela esta ambientada en", ambientada,"y fue publicado a trves de", editor)
+#Agrupacion 62
+estren=2010
+dur=148
+original="Inception"
+print("El origen, de titulo original,", original,"es una pelicula del año",str(estren),"y cuya duracion es de", str(dur),"minutos")
+#Agrupacion 63
+gene="ciencia ficcion psicologica"
+presup=160000000
+distr="Warner Bros. Pictures"
+print("El origen pertenece al grupo de peliculas de", gene, "conto con un presupuesto de", str(presup), "y fue distribuida por",distr)
+#Agrupacion 64
+protg="Leonardo DiCaprio"
+prodcc="Christopher Nolan"
+country="Estados Unidos"
+print("El origen fue protagonizada por", protg, "ademas de ser producida por", prodcc, "y rodada en los", country)
+#Agrupacion 65
+aut="Gabriel García Márquez"
+shets=471
+torial="planeta"
+print("Cien años de soledad es una obra del autor", aut, "cuenta con un total de", str(shets), "y tiene como editorial a", torial)
+#Agrupacion 66
+generoo="novela realismo magico"
+anio_pub=1967
+pic="colombia"
+print("Cien años de soledad es una novela que pertenece al genero", generoo, "fue publicada en el año", anio_pub,"en el pais de", pic)
+
+#fin#
+
+
+
+
+
+
+
+
+
+
+
